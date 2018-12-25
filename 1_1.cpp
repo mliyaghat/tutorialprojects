91fecfe9140988312f57289fa6794a2c423e5ccc
mliyaghat <44662262+mliyaghat@users.noreply.github.com>
2018-12-24 23:44:38 -0800

    Update 1_1.cpp

--- 1_1.cpp
+++ 1_1.cpp
@@ -15,15 +15,15 @@ int main() {
     {
         cout << 0-(c/b);
     }
-    else if(pow(2,b)-4*a*c<0)
+    else if(pow(b,2)-4*a*c<0)
     {
         cout << "invalid value";
     }
     else
     {
         int x1, x2;
-        x1=(0-b)+sqrt(pow(2,b)-4*a*c)/2*a;
-        x2=(0-b)-sqrt(pow(2,b)-4*a*c)/2*a;
+        x1=(0-b)+sqrt(pow(b,2)-4*a*c)/2*a;
+        x2=(0-b)-sqrt(pow(b,2)-4*a*c)/2*a;
         cout << x1;
         cout << x2;
     }
