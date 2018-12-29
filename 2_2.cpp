d4d4a20fa48a9d9aba9ac8bbee1244e054f91f7f
mliyaghat <44662262+mliyaghat@users.noreply.github.com>
2018-12-29 06:30:05 -0800

    Add files via upload

---
+++ 2_2.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+using namespace std;
+ 
+int main() {
+	int n=10;
+	float a;
+	float array [n];
+	float arrayindex [n];
+	for (int i=0;i<n;i++){
+		cin>>array[i];
+		arrayindex[i]=i;
+	}
+	for(int j=0;j<n;j++){
+		for(int i=0;i<n;i++){
+			if (array[i]<array[i+1]){
+				a = array[i];
+	    		array[i] = array[i + 1];
+	      		array[i + 1] = a;
+	      		a = arrayindex[i];
+	    		arrayindex[i] = arrayindex[i + 1];
+	      		arrayindex[i + 1] = a;
+			}
+		}
+	}
+
+	for (int k=0;k<n;k++){
+		if(array[k]>200){
+			cout<<arrayindex[k]<<endl;
+		}
+	}
+}
