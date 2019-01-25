233c64f39c89dccd01853e61254fe8878233ddcc
mliyaghat <44662262+mliyaghat@users.noreply.github.com>
2019-01-25 11:43:03 +0330

    Update 2_2.cpp

--- 2_2.cpp
+++ 2_2.cpp
@@ -3,29 +3,31 @@ using namespace std;
  
 int main() {
 	int n=10;
+	int j=0, m=0;
 	float a;
 	float array [n];
 	float array2 [n];
+	for(int i=0;i<n;i++){
+		array2[i]=0.0;
+	}
 	for (int i=0;i<n;i++){
 		cin>>array[i];
-		array2[i]=array[i];
+		if(array[i]>200){
+			array2[j]=array[i];
+			m++;
+		}
+		j++;
 	}
 	for(int j=0;j<n;j++){
 		for(int i=0;i<n;i++){
-			if (array[i]<array[i+1]){
-				a = array[i];
-	    		array[i] = array[i + 1];
-	      		array[i + 1] = a;
+			if (array2[i]<array2[i+1]){
+				a = array2[i];
+	    		array2[i] = array2[i + 1];
+	      		array2[i + 1] = a;
 			}
 		}
 	}
-	for(int i =0; i<n; i++){
-		if ( array[i]>200){
-    		for(int j=0; j<n; j++){
-        		if (array[i] == array2[j]){
-                	cout<<j<<endl;
-			   }
-		}
+	for(int i=0;i<m;i++){
+		cout<<array2[i]<<endl;
 	}
-}
 }
